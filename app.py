9072609ad9f1aad173d098a6da3ba306ceb081c2
Sujit Kumar Das <35437232+DSujitKumar@users.noreply.github.com>
2024-05-16 16:19:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
 
+
 app = Flask(__name__)
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+os.environ['PORT'] = '8000'
 
 def Wrap_Text_preserve_newlines(text, width=200):
     lines = text.split('\n')
     wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
     wrapped_text = '\n'.join(wrapped_lines)
     return wrapped_text
-
 
 def runInitialData():
     loader = TextLoader("./static/Bhubaneswar.txt",encoding="utf-8")
@@ -37,6 +38,7 @@ embeddeddoc, chain = runInitialData()
 def hello():
     return "Hi There This is your BOT !"
 
+
 @app.route("/getAnswer", methods=['POST', 'GET'])
 def getAnswer():
     Query =  request.args.get('Question')
@@ -44,4 +46,4 @@ def getAnswer():
     return chain.run(input_documents=dc, question=Query)
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=8000)
+    app.run(debug=True,host="0.0.0.0", port=8000)
